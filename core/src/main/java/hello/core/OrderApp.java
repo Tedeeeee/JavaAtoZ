212be6c0c77f4c1990c4479337bd3bb2f2929ae3
Tedeeeee <gksrmf359@gmail.com>
2023-11-29 18:32:13 +0900

    finish sec03

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -10,8 +10,9 @@ import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        OrderService orderService = appConfig.orderService();
+        MemberService memberService = appConfig.memberService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -21,8 +22,6 @@ public class OrderApp {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
         System.out.println("order = " + order.toString());
-        System.out.println("order.calculatePrice() = " + order.calculatePrice());
-        
-        
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
     }
 }
