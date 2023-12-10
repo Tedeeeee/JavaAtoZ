7514240d663879c6f2639e51d50ffa8253a2a24c
Tedeeeee <gksrmf359@gmail.com>
2023-12-10 17:36:39 +0900

    Spring MVC Study Start

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,22 +2,18 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
-
-    @Autowired
-    public LogDemoController(LogDemoService logDemoService, MyLogger myLogger) {
-        this.logDemoService = logDemoService;
-        this.myLogger = myLogger;
-    }
 
     @RequestMapping("log-demo")
     @ResponseBody
