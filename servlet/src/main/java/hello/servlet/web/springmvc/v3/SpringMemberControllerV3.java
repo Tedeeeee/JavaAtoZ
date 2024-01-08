d1cfffc6e9c347e64c87dc33953ed240e48ff0b0
Tedeeeee <gksrmf359@gmail.com>
2024-01-08 23:33:27 +0900

    MVC Structure Study

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,46 @@
+package hello.servlet.web.springmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+    /**
+     * return 값이 viewName 으로 변경되었다.
+     * Model 을 통해 넘겨줄 데이터만 신경쓸 뿐이다.
+     */
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // @RequestMapping(value = "new-form", method = RequestMethod.GET)
+    @GetMapping("new-form")
+    public String newForm() {
+        // return 값을 자동으로 viewName 으로 등록한다.
+        return "new-form";
+    }
+
+    // @RequestMapping(value = "save", method = RequestMethod.POST)
+    @PostMapping("save")
+    public String save(@RequestParam("username") String username, @RequestParam("age") int age, Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    // @RequestMapping(value = "", method = RequestMethod.GET)
+    @GetMapping("")
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
